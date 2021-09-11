5bb76f6a60e5a436011cc4edd3f553afe7bf9722
Zhiqiang-Wu <755960141@qq.com>
2021-09-12 02:39:18 +0800

    actions

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,3 +1,11 @@
 // @author 吴志强
 // @date 2021/9/11
 
+import {createAction} from 'redux-actions';
+import {
+    LIST_ENVIRONMENT_VARIABLES, DELETE_ENVIRONMENT_VARIABLE, SET_ENVIRONMENT_VARIABLE,
+} from '@/actions/actionTypes';
+
+export const listEnvironmentVariables = createAction(LIST_ENVIRONMENT_VARIABLES);
+export const deleteEnvironmentVariable = createAction(DELETE_ENVIRONMENT_VARIABLE);
+export const setEnvironmentVariable = createAction(SET_ENVIRONMENT_VARIABLE);
---
+++ src/models/environmentModel.ts
@@ -0,0 +1,22 @@
+// @author 吴志强
+// @date 2021/9/12
+
+import {
+    deleteEnvironmentVariable, listEnvironmentVariables, setEnvironmentVariable,
+} from '@/services/localService';
+
+export default {
+    namespace: 'environmentModel',
+    state: {},
+    effects: {
+        * listEnvironmentVariables({payload}, {call, put}) {
+            return yield call(listEnvironmentVariables, payload);
+        },
+        * setEnvironmentVariable({payload}, {call}) {
+            return yield call(setEnvironmentVariable, payload);
+        },
+        * deleteEnvironmentVariable({payload}, {call}) {
+            return yield call(deleteEnvironmentVariable, payload);
+        },
+    },
+};
