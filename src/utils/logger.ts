6bf60c0ecf1f92a625650ebf853bfc7cfba6cc06
Zhiqiang-Wu <755960141@qq.com>
2021-09-12 01:08:48 +0800

    日志

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,10 +2,20 @@
 // @date 2021/9/11
 
 import {createLogger, transports, format} from 'winston';
+// import 'winston-daily-rotate-file';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const customFormat = format.printf(({level, message, timestamp}) => {
     return `${timestamp} ${level} - ${message}`;
 });
+
+const transportArray: Array<any> = [
+];
+
+if (isDevelopment) {
+    transportArray.push(new transports.Console());
+}
 
 const logger = createLogger({
     format: format.combine(
@@ -14,9 +24,7 @@ const logger = createLogger({
         }),
         customFormat
     ),
-    transports: [
-        new transports.Console()
-    ]
+    transports: transportArray
 });
 
 export default logger;
