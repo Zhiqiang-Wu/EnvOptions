236933d877186ea745b062e18113184d58cb0794
Zhiqiang-Wu <755960141@qq.com>
2021-09-14 00:35:45 +0800

    托盘

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,7 @@
-import {app, BrowserWindow, protocol} from 'electron';
+// @author 吴志强
+// @date 2021/9/11
+
+import {app, BrowserWindow, Menu, protocol, Tray} from 'electron';
 import createProtocol from 'umi-plugin-electron-builder/lib/createProtocol';
 import path from 'path';
 // import installExtension, {REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} from 'electron-devtools-installer';
@@ -6,6 +9,7 @@ import path from 'path';
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 let mainWindow: BrowserWindow;
+let tray: Tray;
 
 protocol.registerSchemesAsPrivileged([
     {scheme: 'app', privileges: {secure: true, standard: true}},
@@ -23,12 +27,21 @@ const createWindow = () => {
             preload: path.join(__dirname, 'preload.js'),
         },
     });
+
     if (isDevelopment) {
         mainWindow.loadURL('http://localhost:8000');
     } else {
         createProtocol('app');
         mainWindow.loadURL('app://./index.html');
     }
+
+    tray = new Tray(path.join(__dirname, '../../assets/favicon.ico'));
+    const menu = Menu.buildFromTemplate([
+        {label: '退出', click: app.quit}
+    ]);
+    tray.setContextMenu(menu);
+    tray.setToolTip('environment');
+
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
     });
