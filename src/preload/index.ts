d176ba4bbe6cff6792353646d8d110db67a40f2d
Zhiqiang-Wu <755960141@qq.com>
2021-09-19 22:28:20 +0800

    npm

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -3,15 +3,18 @@ import {contextBridge, ipcRenderer} from 'electron';
 const apiKey = 'electron';
 
 const api: any = {
-    versions: process.versions,
-    setEnvironmentVariable: (key: string, value: string): Promise<number> => {
+    // versions: process.versions,
+    setEnvironmentVariable: (key: string, value: string): Promise<Result> => {
         return ipcRenderer.invoke('setEnvironmentVariable', key, value);
     },
-    deleteEnvironmentVariable: (key: string): Promise<number> => {
-        return ipcRenderer.invoke('deleteEnvironmentVariable', key);
+    deleteEnvironmentVariable: (environmentVariable: EnvironmentVariable): Promise<Result> => {
+        return ipcRenderer.invoke('deleteEnvironmentVariable', environmentVariable);
     },
-    listEnvironmentVariables: (): Promise<any> => {
+    listEnvironmentVariables: (): Promise<Result> => {
         return ipcRenderer.invoke('listEnvironmentVariables');
+    },
+    insertEnvironmentVariable: (environmentVariable: EnvironmentVariable): Promise<Result> => {
+        return ipcRenderer.invoke('insertEnvironmentVariable', environmentVariable);
     },
 };
 
