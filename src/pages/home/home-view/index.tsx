25c86e2a14ad4b0ea15119b28f677c0b1b5f170b
Zhiqiang-Wu <755960141@qq.com>
2021-09-21 01:15:22 +0800

    home view

---
+++ src/pages/home/home-view/index.tsx
@@ -0,0 +1,135 @@
+// @author 吴志强
+// @date 2021/9/11
+
+import React, {useEffect} from 'react';
+import {
+    Tooltip, Table, Popconfirm, Typography, Space, Switch, Button, Modal, Form,
+    Input,
+} from 'antd';
+import {DeleteOutlined, EditOutlined, ReloadOutlined} from '@ant-design/icons';
+import styles from './index.scss';
+
+const {Item, useForm} = Form;
+
+const Index = ({
+                      dataSource,
+                      onDelete,
+                      tableLoading,
+                      selectedRowKeys,
+                      selectedOnChange,
+                      visible,
+                      onCancel,
+                      onOk,
+                      onInsert,
+                      onButtonLoading,
+                      onReload,
+                  }: any) => {
+    const columns = [
+        {
+            key: 'key',
+            title: '变量',
+            dataIndex: 'key',
+        },
+        {
+            key: 'type',
+            title: '类型',
+            dataIndex: 'type',
+        },
+        {
+            key: 'value',
+            title: '值',
+            dataIndex: 'value',
+            ellipsis: true,
+        },
+        {
+            key: 'action',
+            title: '操作',
+            width: 80,
+            render: (record) => {
+                return (
+                    <Space>
+                        <Tooltip title='编辑'>
+                            <Typography.Link>
+                                <EditOutlined/>
+                            </Typography.Link>
+                        </Tooltip>
+                        <Popconfirm
+                            title='确认删除？'
+                            onConfirm={() => onDelete(record)}
+                        >
+                            <Tooltip title='删除'>
+                                <Typography.Link>
+                                    <DeleteOutlined/>
+                                </Typography.Link>
+                            </Tooltip>
+                        </Popconfirm>
+                    </Space>
+                );
+            },
+        },
+    ];
+    const [form] = useForm();
+    useEffect(() => {
+        if (!visible) {
+            form.resetFields();
+        }
+    }, [visible]);
+    return (
+        <>
+            <div className={styles.actions}>
+                <Tooltip title='刷新'>
+                    <Button
+                        icon={<ReloadOutlined/>}
+                        disabled={tableLoading}
+                        onClick={onReload}
+                    />
+                </Tooltip>
+                {/*<Switch checkedChildren='类型' unCheckedChildren='类型'/>*/}
+                <Button type='primary' className={styles.insert} onClick={onInsert}>添加</Button>
+            </div>
+            <Table
+                rowKey={(record) => record.id}
+                loading={tableLoading}
+                columns={columns}
+                dataSource={dataSource || []}
+                rowSelection={{
+                    selectedRowKeys,
+                    onChange: selectedOnChange,
+                    hideSelectAll: true,
+                }}
+            />
+            <Modal
+                visible={visible}
+                onCancel={onCancel}
+                onOk={form.submit}
+                centered={true}
+                title='添加'
+                okButtonProps={{loading: onButtonLoading}}
+                // 预渲染
+                // 不执行预渲染的话第一次执行form.resetFields()的时候from找不到Model里的Form，因为还没被渲染
+                forceRender={true}
+            >
+                <Form form={form} labelCol={{span: 4}} onFinish={onOk}>
+                    <Item
+                        name='key'
+                        label='变量名'
+                        required={true}
+                        rules={[{required: true, message: '请输入变量名'}]}
+                    >
+                        <Input/>
+                    </Item>
+                    <Item
+                        name='value'
+                        label='值'
+                        required={true}
+                        rules={[{required: true, message: '请输入值'}]}
+                    >
+                        <Input/>
+                    </Item>
+                </Form>
+            </Modal>
+        </>
+    );
+};
+
+export default Index;
---
+++ src/pages/home/index.ts
@@ -0,0 +1,155 @@
+// @author 吴志强
+// @date 2021/9/11
+
+import {message} from 'antd';
+import {createSelector} from 'reselect';
+import {
+    LIST_ENVIRONMENT_VARIABLES, SET_ENVIRONMENT_VARIABLE, DELETE_ENVIRONMENT_VARIABLE,
+    INSERT_ENVIRONMENT_VARIABLE,
+} from '@/actions/actionTypes';
+import loadsh from 'loadsh';
+import {compose, lifecycle, withState, pure, withHandlers} from 'recompose';
+import HomeView from '@/pages/home/home-view';
+import withDva from '@/components/with-dva';
+import {
+    listEnvironmentVariables, setEnvironmentVariable, deleteEnvironmentVariable,
+    insertEnvironmentVariable,
+} from '@/actions/actions';
+
+interface IProps {
+    dispatch: Function;
+    setDataSource: Function;
+    dataSource: Array<EnvironmentVariable>;
+    setSelectedRowKeys: Function;
+    selectedRowKeys: Array<number>;
+    visible: boolean;
+    setVisible: Function;
+}
+
+const onInsert = (props: IProps) => () => {
+    const {setVisible} = props;
+    setVisible(true);
+};
+
+const onOk = (props: IProps) => (value: {key: string, value: string}) => {
+    const {dispatch, dataSource, setDataSource, setSelectedRowKeys, setVisible} = props;
+    const exists = dataSource.some((environmentVariable) => {
+        return environmentVariable.key === value.key && environmentVariable.value === value.value;
+    });
+    if (exists) {
+        message.warn('已存在');
+        return;
+    }
+    const environmentVariable = {
+        ...value,
+        type: 'REG_SZ',
+    };
+    dispatch(insertEnvironmentVariable(environmentVariable)).then(async (result: Result) => {
+        if (result.code === 200) {
+            setVisible(false);
+            message.success('添加成功');
+            return dispatch(listEnvironmentVariables());
+        } else {
+            return result;
+        }
+    }).then((result: Result) => {
+        if (result.code === 200) {
+            const environmentVariables: Array<EnvironmentVariable> = result.data.environmentVariables;
+            setDataSource(environmentVariables);
+            setSelectedRowKeys(environmentVariables.filter((value => value.selected)).map((value) => value.id));
+        } else {
+            message.warn(result.message);
+        }
+    });
+};
+
+const onCancel = (props: IProps) => () => {
+    const {setVisible} = props;
+    setVisible(false);
+};
+
+const onDelete = (props: IProps) => (environmentVariable: EnvironmentVariable) => {
+    const {dispatch, setDataSource} = props;
+    dispatch(deleteEnvironmentVariable(environmentVariable)).then((result) => {
+        console.log(result);
+    });
+};
+
+const onEdit = (props: IProps) => (event) => {
+
+};
+
+const onReload = (props: IProps) => () => {
+    const {dispatch, setDataSource, setSelectedRowKeys} = props;
+    dispatch(listEnvironmentVariables()).then((result: Result) => {
+        if (result.code === 200) {
+            const environmentVariables: Array<EnvironmentVariable> = result.data.environmentVariables;
+            setDataSource(environmentVariables);
+            setSelectedRowKeys(environmentVariables.filter((value => value.selected)).map((value) => value.id));
+        } else {
+            message.warn(result.message);
+        }
+    });
+};
+
+const selectedOnChange = (props: IProps) => (keys: Array<number>, selectedRows: Array<EnvironmentVariable>) => {
+    const {setSelectedRowKeys, selectedRowKeys, dataSource, dispatch} = props;
+    let environmentVariable;
+    let selected;
+    if (keys.length < selectedRowKeys.length) {
+        // 取消选中的id
+        const id = loadsh.difference(selectedRowKeys, keys)[0];
+        environmentVariable = dataSource.find((value) => value.id === id);
+        selected = false;
+    } else {
+        // 新选中的id
+        const id = loadsh.difference(keys, selectedRowKeys)[0];
+        environmentVariable = selectedRows.find((value) => value.id === id);
+        selected = true;
+    }
+    dispatch(setEnvironmentVariable({...environmentVariable, selected})).then((result: Result) => {
+        if (result.code === 200) {
+            setSelectedRowKeys(keys);
+        } else {
+            message.warn(result.message);
+        }
+    });
+};
+
+const withLifecycle = lifecycle({
+    componentDidMount() {
+        const {dispatch, setDataSource, setSelectedRowKeys}: any = this.props;
+        dispatch(listEnvironmentVariables()).then((result: Result) => {
+            if (result.code === 200) {
+                const environmentVariables: Array<EnvironmentVariable> = result.data.environmentVariables;
+                setDataSource(environmentVariables);
+                setSelectedRowKeys(environmentVariables.filter((value => value.selected)).map((value) => value.id));
+            } else {
+                message.warn(result.message);
+            }
+        });
+    },
+});
+
+const selector = createSelector((state: any) => ({
+    loadings: state.loading.effects,
+}), ({loadings}) => ({
+    tableLoading: loadings[LIST_ENVIRONMENT_VARIABLES] === true
+        || loadings[DELETE_ENVIRONMENT_VARIABLE] === true
+        || loadings[SET_ENVIRONMENT_VARIABLE] === true,
+    onButtonLoading: loadings[INSERT_ENVIRONMENT_VARIABLE] === true,
+}));
+
+const mapStateToProps = (state) => selector(state);
+
+export default compose(
+    withDva(mapStateToProps),
+    withState('dataSource', 'setDataSource', []),
+    withState('selectedRowKeys', 'setSelectedRowKeys', []),
+    withState('visible', 'setVisible', false),
+    withHandlers({
+        onInsert, selectedOnChange, onDelete, onOk, onCancel, onReload
+    }),
+    withLifecycle,
+    pure,
+)(HomeView);
