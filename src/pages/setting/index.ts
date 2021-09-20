16aca9ed5d2c7e1052785e4d08e2b9be6ba30629
Zhiqiang-Wu <755960141@qq.com>
2021-09-21 02:47:00 +0800

    设置

---
+++ src/pages/setting/index.ts
@@ -0,0 +1,7 @@
+// @author 吴志强
+// @date 2021/9/19
+
+import {compose} from 'recompose';
+import SettingView from '@/pages/setting/setting-view';
+
+export default compose()(SettingView);
---
+++ src/pages/setting/setting-view/index.tsx
@@ -0,0 +1,14 @@
+// @author 吴志强
+// @date 2021/9/19
+
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            设置
+        </div>
+    );
+}
+
+export default Index;
