f673f14a2dd5b6126e657653b744151c0171bb71
Zhiqiang-Wu <755960141@qq.com>
2021-09-21 14:06:41 +0800

    actions

--- src/actions/actionTypes.ts
+++ src/actions/actionTypes.ts
@@ -7,3 +7,6 @@ export const SET_ENVIRONMENT_VARIABLE = 'environmentModel/setEnvironmentVariable
 export const INSERT_ENVIRONMENT_VARIABLE = 'environmentModel/insertEnvironmentVariable';
 export const GET_ENVIRONMENT_VARIABLE = 'environmentModel/getEnvironmentVariable';
 export const UPDATE_ENVIRONMENT_VARIABLE = 'environmentModel/updateEnvironmentVariable';
+
+export const UPDATE_SETTING = 'settingModel/updateSetting';
+export const GET_SETTING = 'settingModel/getSetting';
--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,8 +3,14 @@
 
 import {createAction} from 'redux-actions';
 import {
-    LIST_ENVIRONMENT_VARIABLES, DELETE_ENVIRONMENT_VARIABLE, SET_ENVIRONMENT_VARIABLE,
-    INSERT_ENVIRONMENT_VARIABLE, GET_ENVIRONMENT_VARIABLE, UPDATE_ENVIRONMENT_VARIABLE
+    LIST_ENVIRONMENT_VARIABLES,
+    DELETE_ENVIRONMENT_VARIABLE,
+    SET_ENVIRONMENT_VARIABLE,
+    INSERT_ENVIRONMENT_VARIABLE,
+    GET_ENVIRONMENT_VARIABLE,
+    UPDATE_ENVIRONMENT_VARIABLE,
+    UPDATE_SETTING,
+    GET_SETTING
 } from '@/actions/actionTypes';
 
 export const listEnvironmentVariables = createAction(LIST_ENVIRONMENT_VARIABLES);
@@ -13,3 +19,6 @@ export const setEnvironmentVariable = createAction(SET_ENVIRONMENT_VARIABLE);
 export const insertEnvironmentVariable = createAction(INSERT_ENVIRONMENT_VARIABLE);
 export const getEnvironmentVariable = createAction(GET_ENVIRONMENT_VARIABLE);
 export const updateEnvironmentVariable = createAction(UPDATE_ENVIRONMENT_VARIABLE);
+
+export const updateSetting = createAction(UPDATE_SETTING);
+export const getSetting = createAction(GET_SETTING);
