be13a5bf36c9fe47a73b26bfc096f4dbd97daee9
Zhiqiang-Wu <755960141@qq.com>
2021-09-28 14:54:24 +0800

    about

--- src/pages/about/about-view/index.tsx
+++ src/pages/about/about-view/index.tsx
@@ -1,9 +1,20 @@
 // @author 吴志强
 // @date 2021/9/27
 
+import {Typography} from 'antd';
+import styles from './index.scss';
+
 const AboutView = () => {
     return (
-        <div>关于</div>
+        <div className={styles.about}>
+            <div className={styles.header}>
+                <Typography.Title>Env Options</Typography.Title>
+                <Typography.Title level={4} type='secondary'>环境变量管理工具</Typography.Title>
+            </div>
+            <div>
+                version 1.0.0-alpha
+            </div>
+        </div>
     );
 };
 
