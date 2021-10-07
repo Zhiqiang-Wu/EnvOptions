eb1303e8f4b2c5b12c759fe62e5653b8ef7d8083
Zhiqiang-Wu <755960141@qq.com>
2021-10-08 00:03:24 +0800

    dev tools

--- src/main/index.ts
+++ src/main/index.ts
@@ -53,6 +53,7 @@ const createWindow = (): void => {
         show: false,
         title: 'Env Options',
         webPreferences: {
+            devTools: isDevelopment,
             contextIsolation: true,
             preload: path.join(__dirname, 'preload.js'),
         },
