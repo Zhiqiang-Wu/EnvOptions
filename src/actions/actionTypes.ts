12233ca615ad7ef5b5a31801a06a44e30fdb9ba4
Zhiqiang-Wu <755960141@qq.com>
2021-12-02 21:12:58 +0800

    maven

--- src/actions/actionTypes.ts
+++ src/actions/actionTypes.ts
@@ -1,8 +1,6 @@
 // @author 吴志强
 // @date 2021/9/11
 
-import {listDependencies} from '@/services/mavenService';
-
 export const LIST_ENVIRONMENT_VARIABLES = 'environmentModel/listEnvironmentVariables';
 export const DELETE_ENVIRONMENT_VARIABLE = 'environmentModel/deleteEnvironmentVariable';
 export const SET_ENVIRONMENT_VARIABLE = 'environmentModel/setEnvironmentVariable';
