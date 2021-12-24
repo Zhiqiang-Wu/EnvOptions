2d5f7af017b95821a3f99c0d7ce94b75503a3a4d
Zhiqiang-Wu <755960141@qq.com>
2021-12-24 16:41:12 +0800

    scan

--- src/pages/scan/index.ts
+++ src/pages/scan/index.ts
@@ -2,6 +2,81 @@
 // @date 2021/12/24
 
 import ScanView from '@/pages/scan/scan-view';
-import {compose} from 'recompose';
+import {compose, withHandlers, lifecycle, withState} from 'recompose';
+import withDva from '@/components/with-dva';
+import {createSelector} from 'reselect';
+import {updateScanModel, listVideoInputDevices} from '@/actions/actions';
+import {message} from 'antd';
+import {LIST_VIDEO_INPUT_DEVICES} from '@/actions/actionTypes';
+import lodash from 'lodash';
 
-export default compose()(ScanView);
+interface IProps {
+    dispatch: Function;
+    enable: boolean;
+    selectedRowKeys: Array<string>;
+    setSelectedRowKeys: Function;
+}
+
+const onSwitchChange = ({dispatch}: IProps) => (enable) => {
+    dispatch(updateScanModel([
+        {keyPath: ['enable'], value: enable}
+    ]));
+};
+
+const onSelectedChange = ({selectedRowKeys, enable, dispatch}: IProps) => (keys: Array<string>) => {
+    if (enable) {
+        return;
+    }
+    dispatch(updateScanModel([
+        {keyPath: ['selectedRowKeys'], value: lodash.xor(selectedRowKeys, keys)}
+    ]));
+};
+
+const withLifecycle = lifecycle({
+    componentDidMount() {
+        const {dispatch, setVideoInputDevices, selectedRowKeys}: any = this.props;
+        dispatch(listVideoInputDevices()).then((result: Result) => {
+            if (result.code != 200) {
+                message.warn(result.message);
+                return;
+            }
+            const videoInputDevices: Array<VideoInputDevice> = result.data.videoInputDevices;
+            if (videoInputDevices.length > 0) {
+                if (selectedRowKeys.length <= 0) {
+                    dispatch(updateScanModel([
+                        {keyPath: ['selectedRowKeys'], value: [videoInputDevices[0].deviceId]}
+                    ]));
+                }
+                setVideoInputDevices(result.data.videoInputDevices);
+            }
+        });
+    }
+});
+
+const mapStateToProps = (state) => ({
+    enable: createSelector((state: any) => {
+        return state.scanModel.get('enable');
+    }, (enable) => {
+        return enable;
+    })(state),
+    loading: createSelector(() => {
+        return state.loading.effects[LIST_VIDEO_INPUT_DEVICES]
+    }, (loading) => {
+        return loading === true;
+    })(state),
+    selectedRowKeys: createSelector(() => {
+        return state.scanModel.get('selectedRowKeys');
+    }, (selectedRowKeys) => {
+        return selectedRowKeys.toJS();
+    })(state),
+});
+
+export default compose(
+    withDva(mapStateToProps),
+    withState('videoInputDevices', 'setVideoInputDevices', []),
+    withHandlers({
+        onSwitchChange,
+        onSelectedChange,
+    }),
+    withLifecycle,
+)(ScanView);
--- src/pages/scan/scan-view/index.tsx
+++ src/pages/scan/scan-view/index.tsx
@@ -2,11 +2,56 @@
 // @date 2021/12/24
 
 import React from 'react';
+import {Switch, Table} from 'antd';
 
-const ScanView = () => {
+const ScanView = ({
+                      onSelectedChange,
+                      enable,
+                      onSwitchChange,
+                      videoInputDevices,
+                      loading,
+                      selectedRowKeys,
+                  }: any) => {
+    const columns = [
+        {
+            key: 'deviceId',
+            title: 'deviceId',
+            dataIndex: 'deviceId',
+            ellipsis: true,
+        },
+        {
+            key: 'groupId',
+            title: 'groupId',
+            dataIndex: 'groupId',
+            ellipsis: true,
+        },
+        {
+            key: 'label',
+            title: 'label',
+            dataIndex: 'label',
+            ellipsis: true,
+        },
+    ];
     return (
         <div>
-            扫码
+            <Switch
+                disabled={selectedRowKeys.length <= 0}
+                onChange={onSwitchChange}
+                checked={enable}
+                checkedChildren='开启'
+                unCheckedChildren='关闭'
+            />
+            <Table
+                loading={loading}
+                columns={columns}
+                dataSource={videoInputDevices}
+                rowKey={(record) => record.deviceId}
+                rowSelection={{
+                    hideSelectAll: true,
+                    selectedRowKeys,
+                    onChange: onSelectedChange,
+                }}
+            />
         </div>
     );
 };
